1b065152ce2211f75cb2c5f0c3d4f7b5d339479a
Paulo Bruno <paulo-bruno.de-sousa-serafim@inria.fr>
2022-05-14 23:58:46 +0200

    Remove unnecessary functions by improving dispatch organization

--- src/prunelayers.jl
+++ src/prunelayers.jl
@@ -35,53 +35,24 @@ end
 
 numofnonzerostoremove(A::SparseMatrixCSC, n::Integer)::Integer = n - (length(A) - nnz(A))
 
+getelementindexes(A, n, pm::PruneRandomly) = shuffle(eachindex(A.nzval))[1:n]
+getelementindexes(A, n, pm::PruneByQuantity) = partialsortperm(A.nzval, 1:n, by=abs)
+
+
 # TODO: if A contain zeros, the end result could be undesired, since some
 #  zeros may remain and will be erased after dropzeros call
 # TODO: check which is faster (1) dropzeros!(A) followed by nnz(A) or 
 #  (2) count(!iszero, A)
-function droprand!(A::SparseMatrixCSC, p::AbstractFloat)::SparseMatrixCSC
-    @assert zero(p) ≤ p ≤ one(p)
+function dropquantity!(A::SparseMatrixCSC, pm::PruningMethod)::SparseMatrixCSC
+    @assert pm.value isa Integer
+    @assert pm.value ≥ zero(pm.value)
 
-    # num of non-zeros to drop
-    n = round(Integer, p * length(A))
-    
-    return droprand!(A, n)
-end
-
-function droprand!(A::SparseMatrixCSC, n::Integer)::SparseMatrixCSC
-    @assert n ≥ zero(n)
-
-    nzr = numofnonzerostoremove(A, n)
+    nzr = numofnonzerostoremove(A, pm.value)
 
     if nzr > 0
-        @views idx = shuffle(eachindex(A.nzval))[1:nzr]
+        @views idx = getelementindexes(A, nzr, pm)
         A.nzval[idx] .= 0
     end
-
-    return dropzeros!(A)
-end
-
-function droppercentage!(A::SparseMatrixCSC, p::AbstractFloat)::SparseMatrixCSC
-    @assert zero(p) ≤ p ≤ one(p)
-    
-    n = round(Integer, p * length(A))
-    nzr = numofnonzerostoremove(A, n)
-
-    if nzr > 0 
-        @views idx = partialsortperm(A.nzval, 1:nzr, by=abs)
-        A.nzval[idx] .= 0
-    end
-
-    return dropzeros!(A)
-end
-
-function dropquantity!(A::SparseMatrixCSC, n::Integer)::SparseMatrixCSC
-    @assert n ≥ 0
-
-    nzr = numofnonzerostoremove(A, n)
-
-    @views idx = partialsortperm(A.nzval, 1:nzr, by=abs)
-    A.nzval[idx] .= 0
 
     return dropzeros!(A)
 end
@@ -104,8 +75,20 @@ end
 
 # unstructured
 function prunelayer(layer::Dense, pm::PruneRandomly)::Dense
+    @assert pm.value ≥ zero(pm.value)
+    
+    if pm.value isa AbstractFloat
+        if pm.value ≤ one(pm.value)
+            n = round(Integer, pm.value * length(layer.weight))
+        else
+            n = round(Integer, pm.value)
+        end
+        pm = PruneRandomly(n)
+    end
+    
     w = sparse(layer.weight)
-    droprand!(w, pm.value)
+    dropquantity!(w, pm)
+    
     return Dense(w, layer.bias, layer.σ)
 end
 
@@ -116,14 +99,15 @@ function prunelayer(layer::Dense, pm::PruneByMagnitude)::Dense
 end
 
 function prunelayer(layer::Dense, pm::PruneByPercentage)::Dense
-    w = sparse(layer.weight)
-    droppercentage!(w, pm.value)
-    return Dense(w, layer.bias, layer.σ)
+    @assert zero(pm.value) ≤ pm.value ≤ one(pm.value)
+    
+    n = round(Integer, pm.value * length(layer.weight))
+    return prunelayer(layer, PruneByQuantity(n))
 end
 
 function prunelayer(layer::Dense, pm::PruneByQuantity)::Dense
     w = sparse(layer.weight)
-    dropquantity!(w, pm.value)
+    dropquantity!(w, pm)
     return Dense(w, layer.bias, layer.σ)
 end
 
