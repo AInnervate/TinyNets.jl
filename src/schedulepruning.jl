cb610fdf0ae7d120b83dd170a3ab2b8f0c789713
acarvalh <arthur.carvalho-walraven-da-cunha@inria.fr>
2022-11-24 17:13:00 +0100

    Housekeeping

--- src/schedulepruning.jl
+++ src/schedulepruning.jl
@@ -175,16 +175,15 @@ function finetune(model::Any, strategy::TuneByAccuracyDifference, loss::Function
 end
 
 
-function finetune(model::Any,strategy::TuneToConvergence, loss::Function, parameters::Any, optimiser::Flux.Optimise.AbstractOptimiser, data::Any; maxepochs::Integer=100, verbose::Bool=false)
-
-    #separating data
+function finetune(model::Any, strategy::TuneToConvergence, loss::Function, parameters::Any, optimiser::Flux.Optimise.AbstractOptimiser, data::Any; maxepochs::Integer=100, verbose::Bool=false)
+    # Separating dataset
     train, test = splitobs(shuffleobs(data.data), at = 0.5)
-    train_loader=DataLoader(train, batchsize=256)
-    test_loss()=loss(test[1], test[2])
+    train_loader = DataLoader(train, batchsize = 256)
+    test_loss() = loss(test[1], test[2])
     es = early_stopping(test_loss, 2; init_score = 100)
     for epoch ∈ 1:maxepochs
         train!(loss, parameters, train_loader, optimiser)
-        l=loss(test[1], test[2])
+        l = loss(test[1], test[2])
         @info "Epoch $epoch - test loss: $l"
         es() && break
     end
