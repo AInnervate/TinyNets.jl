59772b547aee815e2ce24850074e34ccdc8068df
CARVALHO WALRAVEN DA CUNHA Arthur <arthur.carvalho-walraven-da-cunha@inria.fr>
2022-12-02 11:12:08 +0100

    Bring (duplicate of) utility methods

--- src/prune.jl
+++ src/prune.jl
@@ -3,4 +3,8 @@ module Prune
 using Flux
 
 
+nnz(model) = sum(x->count(!iszero, x), Flux.params(model))
+countparams(model) = sum(length, Flux.params(model))
+sparsity(model) = 1.0 - nnz(model)/countparams(model)
+
 end
