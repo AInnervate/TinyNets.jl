29b8320c24edb875ac0bf1447c4d7f47fc013196
CARVALHO WALRAVEN DA CUNHA Arthur <arthur.carvalho-walraven-da-cunha@inria.fr>
2022-12-02 11:38:28 +0100

    Add function for pruning matrices

--- src/prune.jl
+++ src/prune.jl
@@ -1,10 +1,20 @@
 module Prune
 
 using Flux
+using Random
 
 
 nnz(model) = sum(x->count(!iszero, x), Flux.params(model))
 countparams(model) = sum(length, Flux.params(model))
 sparsity(model) = 1.0 - nnz(model)/countparams(model)
+
+function drop!(A::VecOrMat, qty::Int, by::Function)::VecOrMat
+    @assert 0 ≤ qty ≤ length(A)
+
+    idx = partialsortperm(vec(A), 1:qty, by=by)
+    A[idx] .= zero(eltype(A))
+
+    return A
+end
 
 end
