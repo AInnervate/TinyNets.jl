dde980bb8af6b128967e16ae8570a8068e1ab9f4
CARVALHO WALRAVEN DA CUNHA Arthur <arthur.carvalho-walraven-da-cunha@inria.fr>
2022-12-02 23:46:10 +0100

    Check if the model is already sparse enough

--- src/prune.jl
+++ src/prune.jl
@@ -30,7 +30,15 @@ Layer-wise pruning can be achieved through broadcasting, e.g., `prune!.(model; b
 function prune!(model; by::Function, target_sparsity::Real, verbose::Bool=false)
     @assert 0 ≤ target_sparsity ≤ 1
 
-    verbose && @info @sprintf("Current sparsity: %.1f%%. Pruning to target sparsity: %.1f%%.", 100*sparsity(model), 100*target_sparsity)
+    # Check if the model is already sparse enough
+    current_sparsity = sparsity(model)
+    Δsparsity = target_sparsity - sparsity(model)
+    if Δsparsity ≤ 0
+        verbose && @info @sprintf("Current sparsity (%.1f%%) is already at least the target sparsity (%.1f%%).\n\tNothing to be done.", 100*current_sparsity, 100*target_sparsity)
+        return model
+    end
+
+    verbose && @info @sprintf("Current sparsity: %.1f%%. Pruning to target sparsity: %.1f%%.", 100*current_sparsity, 100*target_sparsity)
     refs = [Ref(p, i) for p in Flux.params(model) for i in eachindex(p)]
     n_toprune = round(Int, target_sparsity * length(refs))
     indices = partialsortperm(refs, 1:n_toprune, by=by∘getindex)
